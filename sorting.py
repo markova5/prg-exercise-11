bd700852a94b56808a5117c41ee9c2612bfc3d59
Marková Alžběta (271382) <271382@vutbr.cz>
2026-04-23 15:15:46 +0200

    commit3

--- sorting.py
+++ sorting.py
@@ -1,4 +1,7 @@
 import random
+import matplotlib.pyplot as plt
+
+
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -21,18 +24,48 @@ def selection_sort(numbers):
 
     return numbers
 
+def bubble_sort(numbers):
+    numbers = numbers.copy()
+
+    m = len(numbers)
+
+    plt.ion()
+    plt.show()
+
+    for cislo_pruchodu in range(m):
+        for j in range(0, m - cislo_pruchodu - 1):
+
+            index_highlight1 = j
+            index_highlight2 = j + 1
+
+            colors = ["steelblue"] * len(numbers)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+
+            plt.clf()
+            plt.bar(range(len(numbers)), numbers, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+
+
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+    plt.ioff()
+    plt.show()
+
+    return numbers
+
 
 
 def main():
     print("Puvodni:", numbers)
     print("Serazeny:", selection_sort(numbers))
 
+    print(bubble_sort(numbers))
+
 
 if __name__ == "__main__":
     main()
-
-
-# def bubble_sort(numbers):
 
 
 
---
+++ student_grades.py
@@ -0,0 +1,9 @@
+class StudentsGrades:
+    def __init__(self, scores):
+        self.scores = scores
+
+    def get_by_index(self, index):
+        return self.scores[index]
+
+    def count(self):
+        return len(self.scores)
